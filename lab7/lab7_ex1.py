410841a6fe9553bec0717e16d83452d6c67a252d
andreicrita <andreicrita@yahoo.com>
2023-11-29 15:43:14 +0200

    Mesaj de commit

--- lab7/lab7_ex1.py
+++ lab7/lab7_ex1.py
@@ -1,15 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-N = 5000
+N = 512
 
-# Definirea funcțiilor
 n1, n2 = np.meshgrid(np.arange(0, N), np.arange(0, N))
 
 # Subpunctul a)
 x_a = np.sin(2*np.pi*n1 + 3*np.pi*n2)
 
-# Fereastra pentru imaginea și spectrul din a)
 plt.figure(figsize=(12, 6))
 
 plt.subplot(1, 2, 1)
@@ -21,7 +19,7 @@ Y_a = np.fft.fft2(x_a)
 freq_db_a = 20*np.log10(np.abs(np.fft.fftshift(Y_a)))
 plt.imshow(freq_db_a)
 plt.colorbar()
-plt.title('a) Spectrul în scală logaritmică')
+plt.title('a) Spectrul')
 
 plt.tight_layout()
 plt.show()
@@ -29,7 +27,6 @@ plt.show()
 # Subpunctul b)
 x_b = np.sin(4*np.pi*n1) + np.cos(6*np.pi*n2)
 
-# Fereastra pentru imaginea și spectrul din b)
 plt.figure(figsize=(12, 6))
 
 plt.subplot(1, 2, 1)
@@ -41,7 +38,7 @@ Y_b = np.fft.fft2(x_b)
 freq_db_b = 20*np.log10(np.abs(np.fft.fftshift(Y_b)))
 plt.imshow(freq_db_b)
 plt.colorbar()
-plt.title('b) Spectrul în scală logaritmică')
+plt.title('b) Spectrul')
 
 plt.tight_layout()
 plt.show()
@@ -51,19 +48,18 @@ Y_c = np.zeros_like(n1, dtype=complex)
 Y_c[0, 5] = 1
 Y_c[0, -5] = 1
 
-# Fereastra pentru imaginea și spectrul din c)
 plt.figure(figsize=(12, 6))
 
 plt.subplot(1, 2, 1)
 X_c = np.fft.ifft2(Y_c)
 X_c = np.real(X_c)
 plt.imshow(X_c, cmap=plt.cm.gray)
-plt.title('c) Imaginea pentru Y(m1, m2) conform specificațiilor date')
+plt.title('c) Imaginea pentru Y(0, 5)')
 
 plt.subplot(1, 2, 2)
 plt.imshow(20*np.log10(np.abs(np.fft.fftshift(Y_c))))
 plt.colorbar()
-plt.title('c) Spectrul în scală logaritmică')
+plt.title('c) Spectrul')
 
 plt.tight_layout()
 plt.show()
@@ -80,12 +76,12 @@ plt.subplot(1, 2, 1)
 X_d = np.fft.ifft2(Y_d)
 X_d = np.real(X_d)
 plt.imshow(X_d, cmap=plt.cm.gray)
-plt.title('d) Imaginea pentru Y(m1, m2) conform specificațiilor date')
+plt.title('d) Imaginea pentru Y(5,0)')
 
 plt.subplot(1, 2, 2)
 plt.imshow(20*np.log10(np.abs(np.fft.fftshift(Y_d))))
 plt.colorbar()
-plt.title('d) Spectrul în scală logaritmică')
+plt.title('d) Spectrul')
 
 plt.tight_layout()
 plt.show()
@@ -95,19 +91,18 @@ Y_e = np.zeros_like(n1, dtype=complex)
 Y_e[5, 5] = 1
 Y_e[-5, -5] = 1
 
-# Fereastra pentru imaginea și spectrul din e)
 plt.figure(figsize=(12, 6))
 
 plt.subplot(1, 2, 1)
 X_e = np.fft.ifft2(Y_e)
 X_e = np.real(X_e)
 plt.imshow(X_e, cmap=plt.cm.gray)
-plt.title('e) Imaginea pentru Y(m1, m2) conform specificațiilor date')
+plt.title('e) Imaginea pentru Y(5, 5)')
 
 plt.subplot(1, 2, 2)
 plt.imshow(20*np.log10(np.abs(np.fft.fftshift(Y_e))))
 plt.colorbar()
-plt.title('e) Spectrul în scală logaritmică')
+plt.title('e) Spectrul')
 
 plt.tight_layout()
 plt.show()
